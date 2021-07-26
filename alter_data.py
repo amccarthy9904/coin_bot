705fb5f850091191e99de1faf15e17e848f4d61f
Aaron McCarthy <amccarthy9904@gmail.com>
2021-07-25 17:58:55 -0700

    script to modify csv files

--- alter_data.py
+++ alter_data.py
@@ -1,10 +1,32 @@
 import csv
 
 
-updata = open('data/BTC-USD_new.csv', 'w+')
 
-with open('data/BTC-USD_orig.csv',newline='') as olddata:
 
-    reader = csv.reader(olddata)
-    for (d,o,h,l,c,a,v) in reader:
-        updata.write(f"{o},{h},{l},{c},{v},{int(float(o) > float(c))}\n")
+def filter_orig():
+    """Filters original dataset to get what we need"""
+
+    filterd_data = open('data/BTC-USD_new.csv', 'w+')
+
+    with open('data/BTC-USD_orig.csv',newline='') as olddata:
+
+        reader = csv.reader(olddata)
+        for (d,o,h,l,c,a,v) in reader:
+            if d == 'Date':
+                continue
+            print(d)
+            filterd_data.write(f"{o},{h},{l},{c},{v},{int(float(c) > float(o))}\n")
+
+
+def create_inputs():
+
+    inputs = open('data/BTC-USD_in.csv', 'w+')
+    with open('data/BTC-USD_new.csv',newline='') as filterd_data:
+        reader = csv.reader(filterd_data)
+        for ind, (o,h,l,c,v,l) in enumerate(reader):
+            
+            if ind < 50:
+                continue
+        # stack 50 prev rows into one row in BTC-USD_in.csv
+
+create_inputs()
