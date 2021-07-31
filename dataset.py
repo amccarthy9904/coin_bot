9166975e4a58bee52bec0f9f7e7697631c4513fa
Aaron McCarthy <amccarthy9904@gmail.com>
2021-07-30 17:52:16 -0700

    reflects final format of training data, removes redundant imports

--- dataset.py
+++ dataset.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import numpy as np
-
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
-
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
-
-
 
 class Dataset:
 
     data_set = None
     data_labels = None
+    input_path = 'data/BTC-USD_in.csv'
 
     def __init__(self) -> None:
         pass
 
     def load_data(self):
-        data_frame = pd.read_csv('data/BTC-USD.csv', names=['Open','High','Low','Close','Volume','Green'])
-        self.data_labels = data_frame.pop('Green')
+        data_frame = pd.read_csv(self.input_path, header=0)
+        self.data_labels = np.array(data_frame.pop('Label'))
         self.data_set = np.array(data_frame)
         print(data_frame.head())
