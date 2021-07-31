bfe8016048df17824a3c701378fa1374476d45ee
Aaron McCarthy <amccarthy9904@gmail.com>
2021-07-30 17:52:55 -0700

    adds class structure and input layer, still very WIP

--- model.py
+++ model.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.python.ops.gen_array_ops import shape
 
+class Model():
+
+    mod = None
+
+    def __init__(self) -> None:
+        
+        self.mod = tf.keras.Sequential()
+        self.mod.add(tf.keras.Input(shape=(250,)))
+        self.mod.add(layers.Dense)
+        self.mod.compile()
 
