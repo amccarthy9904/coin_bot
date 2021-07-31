0af8058ad112058b2cf993016036f523a98e176e
Aaron McCarthy <amccarthy9904@gmail.com>
2021-07-30 17:50:52 -0700

    creates final format of data, 251 items per row, 250 OHLC for prev 50 days, 1 binary label for up or down tomorrow

--- alter_data.py
+++ alter_data.py
@@ -19,14 +19,69 @@ def filter_orig():
 
 
 def create_inputs():
-
+    """
+        Groups data into rows that will be used as input
+        each row represents the previous 50 days
+        the last datapoint in each row is the label for today
+    """
     inputs = open('data/BTC-USD_in.csv', 'w+')
+    prev_50 = []
+
     with open('data/BTC-USD_new.csv',newline='') as filterd_data:
         reader = csv.reader(filterd_data)
-        for ind, (o,h,l,c,v,l) in enumerate(reader):
-            
+
+        for ind, (o,h,l,c,v,label) in enumerate(reader):
+
+            if ind > 50:
+                input_row = ""
+                for day in prev_50:
+                    input_row += day
+                input_row += label
+                inputs.write(f"{input_row}\n")
+
+            prev_50.append(f"{o},{h},{l},{c},{v},")
             if ind < 50:
                 continue
-        # stack 50 prev rows into one row in BTC-USD_in.csv
+            prev_50.pop(0)
 
-create_inputs()
+def create_col_labeled_inputs():
+    """
+        Groups data into rows that will be used as input
+        each row represents the previous 50 days
+        the last datapoint in each row is the label(Green/Red) for today
+        Adds labels for columns as well to help later
+    """
+
+    inputs = open('data/BTC-USD_in.csv', 'w+')
+
+    col_labels = ""
+    for ind in range(250):
+        col_labels += f"{str(ind)},"
+    col_labels += "Label\n"
+
+    print("here")
+    print(col_labels)
+
+    inputs.write(col_labels)
+    prev_50 = []
+
+    with open('data/BTC-USD_new.csv',newline='') as filterd_data:
+        reader = csv.reader(filterd_data)
+
+        for ind, (o,h,l,c,v,label) in enumerate(reader):
+
+            if ind > 50:
+                input_row = ""
+                for day in prev_50:
+                    input_row += day
+                input_row += label
+                inputs.write(f"{input_row}\n")
+
+            prev_50.append(f"{o},{h},{l},{c},{v},")
+            if ind < 50:
+                continue
+            prev_50.pop(0)
+
+
+# create_inputs()
+create_col_labeled_inputs()
